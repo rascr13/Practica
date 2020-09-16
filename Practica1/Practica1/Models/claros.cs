2b36a3ddc2bd3944f2f79eeaaf6fa9a68c5f3b4a
Raul Claros <a2019117860@estudiantes.upsa.edu.bo>
2020-09-15 21:38:32 -0400

    Termine el modelo

---
+++ Practica1/Practica1/Models/claros.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Practica1.Models
+{
+
+    public enum placeType
+    {
+        Burtown,
+        Paradise,
+        Laplaza,
+        Panorama,
+        Gatsby
+    }
+    public class claros
+    {
+        [Key]
+        public int clarosID { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre Completo")]
+        [StringLength(24,MinimumLength = 2)]
+        public string Friendofclaros { get; set; }
+
+        [Required]
+        public placeType Place { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Display(Name = "Cumpleaños")]
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}")]
+        [DataType(DataType.Date)]
+        public DateTime Birthdate { get; set; }
+
+
+
+
+    }
+}

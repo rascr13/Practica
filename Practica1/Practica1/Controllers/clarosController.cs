c86edb91c176af5952ebc57f706c2845b22e341e
Raul Claros <a2019117860@estudiantes.upsa.edu.bo>
2020-09-17 23:06:33 -0400

    controlador creador

---
+++ Practica1/Practica1/Controllers/clarosController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Practica1.Models;
+
+namespace Practica1.Controllers
+{
+    public class clarosController : ApiController
+    {
+        private DataContext db = new DataContext();
+
+        // GET: api/claros
+        public IQueryable<claros> Getclaros()
+        {
+            return db.claros;
+        }
+
+        // GET: api/claros/5
+        [ResponseType(typeof(claros))]
+        public IHttpActionResult Getclaros(int id)
+        {
+            claros claros = db.claros.Find(id);
+            if (claros == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(claros);
+        }
+
+        // PUT: api/claros/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Putclaros(int id, claros claros)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != claros.clarosID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(claros).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!clarosExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/claros
+        [ResponseType(typeof(claros))]
+        public IHttpActionResult Postclaros(claros claros)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.claros.Add(claros);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = claros.clarosID }, claros);
+        }
+
+        // DELETE: api/claros/5
+        [ResponseType(typeof(claros))]
+        public IHttpActionResult Deleteclaros(int id)
+        {
+            claros claros = db.claros.Find(id);
+            if (claros == null)
+            {
+                return NotFound();
+            }
+
+            db.claros.Remove(claros);
+            db.SaveChanges();
+
+            return Ok(claros);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool clarosExists(int id)
+        {
+            return db.claros.Count(e => e.clarosID == id) > 0;
+        }
+    }
+}
---
+++ Practica1/Practica1/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Practica1.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<Practica1.Models.claros> claros { get; set; }
+    }
+}
